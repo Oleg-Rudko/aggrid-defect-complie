0d7cc1719e22bf0111621ea20520b004194d00b8
Oleh Rudko <oleg.rudko0906@gmail.com>
2023-08-23 21:07:27 +0300

    Oleh-Rudko | commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { ModuleRegistry } from "@ag-grid-community/core";
+import { ClientSideRowModelModule } from "@ag-grid-community/client-side-row-model";
+import { ColumnsToolPanelModule } from "@ag-grid-enterprise/column-tool-panel";
+import { FiltersToolPanelModule } from "@ag-grid-enterprise/filter-tool-panel";
+import { RowGroupingModule } from "@ag-grid-enterprise/row-grouping";
+import { CsvExportModule } from "@ag-grid-community/csv-export";
+import { ExcelExportModule } from "@ag-grid-enterprise/excel-export";
+import { SetFilterModule } from "@ag-grid-enterprise/set-filter";
+import { MenuModule } from "@ag-grid-enterprise/menu";
+import { RangeSelectionModule } from "@ag-grid-enterprise/range-selection";
+import { GridChartsModule } from "@ag-grid-enterprise/charts";
+import { AgChart } from "ag-charts-react";
+
+ModuleRegistry.registerModules([
+  ClientSideRowModelModule,
+  RowGroupingModule,
+  CsvExportModule,
+  ExcelExportModule,
+  SetFilterModule,
+  MenuModule,
+  ColumnsToolPanelModule,
+  FiltersToolPanelModule,
+  RangeSelectionModule,
+  GridChartsModule,
+]);
 
 function App() {
   return (
@@ -9,6 +34,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <AgChart options={[]} />
         <a
           className="App-link"
           href="https://reactjs.org"
